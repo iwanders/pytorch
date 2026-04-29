cc5cc10edacaa7e9dce77b915f10baf8ff416837
Ivor Wanders <iwanders@users.noreply.github.com>
2026-04-29 19:02:50 -0400

    Correct naming for exception state shims.
    Co-authored-by: Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>

--- torch/csrc/shim_exception_state.h
+++ torch/csrc/shim_exception_state.h
@@ -3,23 +3,23 @@
 #include <string>
 
 /// Store an exception and its backtrace that occurred in the calling thread.
-AOTI_TORCH_EXPORT void torch_exception_state_set_what(
+AOTI_TORCH_EXPORT void set_torch_exception_what(
     const std::string& our_message);
 
 /// Store an exception that occurred in the calling thread.
-AOTI_TORCH_EXPORT void torch_exception_state_set_what_without_backtrace(
+AOTI_TORCH_EXPORT void set_torch_exception_what_without_backtrace(
     const std::string& our_message);
 
 /// Retrieves the exception and backtrace that was stored in this thread.
 /// This reference is only valid while the calling thread exists and no other
 /// exception is stored for the thread.
-AOTI_TORCH_EXPORT const std::string& torch_exception_state_get_what();
+AOTI_TORCH_EXPORT const std::string& get_torch_exception_what();
 
 /// Retrieves the exception that was stored in this thread.
 /// This reference is only valid while the calling thread exists and no other
 /// exception is stored for the thread.
 AOTI_TORCH_EXPORT const std::string&
-torch_exception_state_get_what_without_backtrace();
+get_torch_exception_what_without_backtrace();
 
 /// Configures the backtrace printing state for the calling thread. Returns the
 /// previous state.
